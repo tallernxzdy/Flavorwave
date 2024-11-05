b5e3498e849716585cc75cb9568dab3389a5fb88
13c-darida <13c-darida@ipari.vein.hu>
2024-11-05 09:24:42 +0100

    kezdolap elkezdese, navbar elkeszitese

---
+++ kezdolap.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.scss">
+</head>
+<body>
+    
+<ul class="horizontal">
+    <li>
+        <a class="active" href="">Home</a>
+    </li>
+    <li>
+        <a href="">News</a>
+    </li>
+    <li>
+        <a href="">Contact</a>
+    </li>
+    <li class="rightli" style="float:right">
+        <a href="">Menü</a>
+    </li>
+</ul>
+
+<h2>Üdvözöljük a Flavorwave oldalon</h2>
+<p><a href="regisztracio.php">Regisztráció</a></p>
+<p><a href="bejelentkezes.php">Bejelentkezes</a></p>
+
+</body>
+</html>
