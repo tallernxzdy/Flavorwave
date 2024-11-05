a237107c7ea15e5e5ceb5d10030253970af9e222
13c-darida <13c-darida@ipari.vein.hu>
2024-11-05 10:49:03 +0100

    kezdolap kezdeti

--- kezdolap.php
+++ kezdolap.php
@@ -5,28 +5,43 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="style.scss">
 </head>
 <body>
     
 <ul class="horizontal">
     <li>
-        <a class="active" href="">Home</a>
+        <a class="navbar" href="kezdolap.php">Főoldal</a>
     </li>
     <li>
-        <a href="">News</a>
+        <a class="navbar" href="menu.php">Menü</a>
     </li>
     <li>
-        <a href="">Contact</a>
+        <a class="navbar bejelentkezés float right" href="bejelentkezes.php">bejelentkezés</a>
     </li>
-    <li class="rightli" style="float:right">
-        <a href="">Menü</a>
+    <li>
+         <a class="navbar regisztráció float right" href="regisztracio.php">regisztráció</a>
+    </li>
+    <li>
+        <a class="navbar kijelentkezés jobbra_csusztatas" href="kijelentkezes.php">kijelentkezés</a>
     </li>
 </ul>
 
 <h2>Üdvözöljük a Flavorwave oldalon</h2>
 <p><a href="regisztracio.php">Regisztráció</a></p>
 <p><a href="bejelentkezes.php">Bejelentkezes</a></p>
+
+
+<?php
+session_start();
+?>
+
+    <?php if (isset($_SESSION["username"])): ?>
+        <p>Szia, <?php echo htmlspecialchars($_SESSION["username"]); ?>!</p>
+        <p><a href="kijelentkezes.php">Kijelentkezés</a></p>
+    <?php else: ?>
+        <p>Jelentkezz be vagy regisztrálj!</p>
+        <p><a href="bejelentkezes.php">Bejelentkezés</a> | <a href="regisztracio.php">Regisztráció</a></p>
+    <?php endif; ?>
 
 </body>
 </html>
