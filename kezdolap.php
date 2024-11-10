984c4501f095f8651e79efa41dafdc436be00ff3
zazussss1 <szitasizalan0624@gmail.com>
2024-11-10 10:51:17 +0100

    navbar design

--- kezdolap.php
+++ kezdolap.php
@@ -1,47 +1,41 @@
+<?php 
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Flavorwave</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
-<ul class="navbar_ul">
-    <li>
-        <a class="navbar" href="kezdolap.php">Főoldal</a>
-    </li>
-    <li>
-        <a class="navbar" href="menu.php">Menü</a>
-    </li>
-    <li>
-        <a class="navbar bejelentkezes float right" href="bejelentkezes.php">bejelentkezés</a>
-    </li>
-    <li>
-         <a class="navbar regisztracio float right" href="regisztracio.php">regisztráció</a>
-    </li>
-    <li>
-        <a class="navbar kijelentkezés jobbra_csusztatas" href="kijelentkezes.php">kijelentkezés</a>
-    </li>
-</ul>
 
-<h2>Üdvözöljük a Flavorwave oldalon</h2>
-<p><a href="regisztracio.php">Regisztráció</a></p>
-<p><a href="bejelentkezes.php">Bejelentkezes</a></p>
+    <!-- NAVBAR -->
+    <nav class="navbar">
+        <ul class="navbar_ul">
+            <li><a class="navbar_link" href="kezdolap.php">Főoldal</a></li>
+            <li><a class="navbar_link" href="menu.php">Menü</a></li>
+            
+            <!-- Jobb oldal: Bejelentkezés/Regisztráció vagy Kijelentkezés -->
+            <div class="right_links">
+                <?php if (isset($_SESSION["username"])): ?>
+                    <li><a class="navbar_link logout" href="kijelentkezes.php">Kijelentkezés</a></li>
+                <?php else: ?>
+                    <li><a class="navbar_link login" href="bejelentkezes.php">Bejelentkezés</a></li>
+                    <li><a class="navbar_link register" href="regisztracio.php">Regisztráció</a></li>
+                <?php endif; ?>
+            </div>
+        </ul>
+    </nav>
 
-
-<?php
-session_start();
-?>
-
+    <!-- Üdvözlő üzenet -->
+    <h2>Üdvözöljük a Flavorwave oldalon</h2>
     <?php if (isset($_SESSION["username"])): ?>
         <p>Szia, <?php echo htmlspecialchars($_SESSION["username"]); ?>!</p>
-        <p><a href="kijelentkezes.php">Kijelentkezés</a></p>
     <?php else: ?>
-        <p>Jelentkezz be vagy regisztrálj!</p>
-        <p><a href="bejelentkezes.php">Bejelentkezés</a> | <a href="regisztracio.php">Regisztráció</a></p>
+        <p>Jelentkezz be vagy regisztrálj a fiókod eléréséhez!</p>
     <?php endif; ?>
-
 </body>
 </html>
