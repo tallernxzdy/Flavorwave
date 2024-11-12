e59ea246273026f95e57be0819592cc952ab3c7e
zazussss1 <szitasizalan0624@gmail.com>
2024-11-12 17:55:54 +0100

    footer

--- php/kezdolap.php
+++ php/kezdolap.php
@@ -17,8 +17,8 @@ session_start();
         <div class="navbar-container">
             <a href="kezdolap.php" class="logo">🌊 Flavorwave</a>
             <ul class="navbar_ul">
-                <li><a class="navbar_link special_link" href="kezdolap.php">Főoldal</a></li>
-                <li><a class="navbar_link special_link" href="menu.php">Menü</a></li>
+                <li><a class="navbar_link special_link kozepso" href="kezdolap.php">Főoldal</a></li>
+                <li><a class="navbar_link special_link kozepso" href="menu.php">Menü</a></li>
             </ul>
             <div class="right_links">
                 <?php if (isset($_SESSION["username"])): ?>
@@ -31,12 +31,38 @@ session_start();
         </div>
     </nav>
 
-    <!-- Üdvözlő üzenet -->
-    <h2>Üdvözöljük a Flavorwave oldalon</h2>
+<!-- Üdvözlő üzenet -->
+<h2>Üdvözöljük a Flavorwave oldalon</h2>
     <?php if (isset($_SESSION["username"])): ?>
         <p>Szia, <?php echo htmlspecialchars($_SESSION["username"]); ?>!</p>
     <?php else: ?>
         <p>Jelentkezz be vagy regisztrálj a fiókod eléréséhez!</p>
     <?php endif; ?>
+
+
+
+
+    <!--Footer-->
+        <footer class="footer">
+            <div class="footer-container">
+                <ul class="footer-links">
+                    <li><a href="#">Rólunk</a></li>
+                    <li><a href="#">Szolgáltatások</a></li>
+                    <li><a href="#">Kapcsolat</a></li>
+                    <li><a href="#">Blog</a></li>
+                </ul>
+                <div class="footer-socials">
+                    <a href="#" target="_blank"><i class="fab fa-facebook"></i></a>
+                    <a href="#" target="_blank"><i class="fab fa-twitter"></i></a>
+                    <a href="#" target="_blank"><i class="fab fa-linkedin"></i></a>
+                    <a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
+                </div>
+                <div class="footer-copy">
+                    <p>&copy; 2024 <a href="#">Flavorwave</a>. Minden jog fenntartva.</p>
+                </div>
+            </div>
+        </footer>
+
+    
 </body>
 </html>
