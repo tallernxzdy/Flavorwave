111b5a971dbb7b42a9933be0a822f2135db9595b
tallernxzdy <adam74.gamer@gmail.com>
2024-11-12 10:08:22 +0100

    asd

--- php/kezdolap.php
+++ php/kezdolap.php
@@ -8,7 +8,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Flavorwave</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body>
 
@@ -17,15 +17,15 @@ session_start();
         <div class="navbar-container">
             <a href="kezdolap.php" class="logo">🌊 Flavorwave</a>
             <ul class="navbar_ul">
-                <li><a class="navbar_link special_link" href="./php/kezdolap.php">Főoldal</a></li>
+                <li><a class="navbar_link special_link" href="kezdolap.php">Főoldal</a></li>
                 <li><a class="navbar_link special_link" href="menu.php">Menü</a></li>
             </ul>
             <div class="right_links">
                 <?php if (isset($_SESSION["username"])): ?>
-                    <a class="navbar_link logout" href="./php/kijelentkezes.php">Kijelentkezés</a>
+                    <a class="navbar_link logout" href="kijelentkezes.php">Kijelentkezés</a>
                 <?php else: ?>
-                    <a class="navbar_link login" href="./php/bejelentkezes.php">Bejelentkezés</a>
-                    <a class="navbar_link register" href="./php/regisztracio.php">Regisztráció</a>
+                    <a class="navbar_link login" href="bejelentkezes.php">Bejelentkezés</a>
+                    <a class="navbar_link register" href="regisztracio.php">Regisztráció</a>
                 <?php endif; ?>
             </div>
         </div>
