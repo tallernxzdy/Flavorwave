9868c6d6339fa571f3478e294e4ebec3e2ba83e9
13c-darida <13c-darida@ipari.vein.hu>
2024-11-12 10:41:31 +0100

    asd

--- php/kezdolap.php
+++ php/kezdolap.php
@@ -8,7 +8,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Flavorwave</title>
-    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
 
