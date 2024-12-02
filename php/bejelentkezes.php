66d71e36f5968037ae9887f88f1e087cd0ccd319
zazussss1 <szitasizalan0624@gmail.com>
2024-12-02 13:03:43 +0100

    orai munka 12.12 (carousel, css)

--- php/bejelentkezes.php
+++ php/bejelentkezes.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="../css/bejelentkezes.css">
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/navbar.css">
+    <link rel="stylesheet" href="../css/footer.css">
 </head>
 <body>
     <!-- Navbar -->
