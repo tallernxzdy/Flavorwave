38a68ba55eabb933252822d278b87481002ee23b
13c-darida <13c-darida@ipari.vein.hu>
2025-03-23 12:40:08 +0100

    dolgozoifelulet semmiseg

--- php/dolgozoi_felulet.php
+++ php/dolgozoi_felulet.php
@@ -77,8 +77,7 @@ if (isset($_POST['finished_order'])) {
     <title>Dolgozói Felület</title>
     <!-- CSS fájok -->
     <link rel="stylesheet" href="../css/navbar.css">
-    <link rel="stylesheet" href="../css/admin_felulet.css">
-    <link rel="stylesheet" href="../css/dolgozoi_felulet.css">
+    <link rel="stylesheet" href="../css/fooldal/ujfooldal.css">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -155,7 +154,7 @@ if (isset($_POST['finished_order'])) {
         </ul>
     </div>
 
-    <div class="container">
+    <div class="container mt-5">
         <h1 class="text-center my-4">Dolgozói felület</h1>
 
         <!-- Átvételre váró rendelések -->
